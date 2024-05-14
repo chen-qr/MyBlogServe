d7afc4cc5ab6de0c67ef8c553c7a4ad501b74525
Alex <alex.chenqr@gmail.com>
2024-05-14 17:04:13 +0800

    add

---
+++ src/main/java/com/chenqr/MyBlogServe/BlogController.java
@@ -0,0 +1,13 @@
+package com.chenqr.MyBlogServe;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Blog!";
+    }
+}
